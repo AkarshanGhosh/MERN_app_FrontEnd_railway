57f7633dced47e42c8145826c639029819a510a0
AkarshanGhosh <ronighosh494@gmail.com>
2024-10-11 17:36:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 // App.js
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar';
-import Home from './components/home';
-import SignUp from './components/signup';
-import SignIn from './components/signin';
-import Contact from './components/Contact'; // Import the Contact component
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import SignUp from "./components/signup";
+import SignIn from "./components/signin";
+import Contact from "./components/Contact"; // Import the Contact component
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/signup" element={<SignUp />} />
-        <Route exact path="/signin" element={<SignIn />} />
-        <Route exact path="/contact" element={<Contact />} /> {/* Add Contact route */}
-      </Routes>
-    </Router>
+    <tableState>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/signin" element={<SignIn />} />
+          <Route exact path="/contact" element={<Contact />} />{" "}
+          {/* Add Contact route */}
+        </Routes>
+      </Router>
+    </tableState>
   );
 }
 
