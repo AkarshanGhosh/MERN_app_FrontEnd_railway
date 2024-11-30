b091f6c6f297c7c5aebb2daf95e4545ad346ae11
AkarshanGhosh <ronighosh494@gmail.com>
2024-12-01 03:31:44 +0530

    updating api

--- src/components/signin.js
+++ src/components/signin.js
@@ -36,7 +36,7 @@ const SignIn = () => {
     };
 
     try {
-      const response = await fetch(`http://localhost:5000/api/auth/login`, {
+      const response = await fetch(`http://51.20.2.163:5000/api/auth/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
