b8c0d37e091756d307d4950fab998967acec887b
AkarshanGhosh <ronighosh494@gmail.com>
2024-11-30 21:16:41 +0530

    updating api

--- src/components/signin.js
+++ src/components/signin.js
@@ -22,7 +22,7 @@ const SignIn = () => {
     };
 
     try {
-      const response = await fetch(`http://localhost:5000/api/auth/login`, {
+      const response = await fetch(`https://mern-app-railway.vercel.app/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
